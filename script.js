0e76525da5e589310dbf779bd8b302efc017042e
wyartb <wyattbombara3@gmail.com>
2026-07-21 16:58:52 -0400

    remove debug log

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function connectLanyard() {
   ws.onmessage = (e) => {
     const msg = JSON.parse(e.data);
     if (msg.op === 0) {
-      console.log('lanyard data:', msg.d);
       updateUI(msg.d);
     }
   };
